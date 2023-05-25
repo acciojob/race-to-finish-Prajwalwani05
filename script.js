763628e48ee257d71f8d293e4ee9a964208e5af1
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-25 16:07:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+for (let i = 0; i < 5; i++) {
+  let randomTime = Math.floor(Math.random() * 5) + 1;
+  let p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(randomTime);
+    }, randomTime * 1000);
+  });
+  promises.push(p);
+}
+
+Promise.any(promises)
+  .then((result) => {
+    document.getElementById("output").innerText = result;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
